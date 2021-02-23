c3064c6f86c8e488bfbe08a82cf8494f6d22636e
Jawayria <jawayriahashmi@gmail.com>
2021-02-23 20:28:11 +0500

    Adding ratelimit

--- openedx/core/djangoapps/user_api/accounts/userprofile_api.py
+++ openedx/core/djangoapps/user_api/accounts/userprofile_api.py
@@ -2,23 +2,16 @@
     API for UserProfile model
 """
 
-from edx_rest_framework_extensions.paginators import DefaultPagination
-import edx_api_doc_tools as apidocs
-from rest_framework import permissions, status
-from rest_framework.authentication import SessionAuthentication
-from rest_framework.parsers import JSONParser
 from rest_framework.response import Response
-from rest_framework.serializers import ValidationError
 from rest_framework.views import APIView
-from rest_framework.viewsets import ViewSet
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.pagination import PageNumberPagination
 
 
-from common.djangoapps.student.models import (  # lint-amnesty, pylint: disable=unused-import
-    User,
+from common.djangoapps.student.models import (
     UserProfile,
 )
+from django.utils.decorators import method_decorator
 from .serializers import UserProfileSerializer
 
 
@@ -33,6 +26,7 @@ class UserProfileAPIView(APIView):
     pagination_class = UsersPagination
     queryset=''
 
+    @method_decorator(ratelimit(key='user', rate='1/m', method='GET'))
     def get(self, request, pk=None):
         page = UsersPagination()
         if pk is not None:
